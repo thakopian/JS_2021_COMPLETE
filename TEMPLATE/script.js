bd9feee2f4b7d905868456533c766bec30dc18d3
Tadeh Hakopian <47580016+thakopian@users.noreply.github.com>
2021-04-20 18:04:40 -0700

    VS Code extensions setup
    Added prettier, snippets and other extensions into VS code environment

---
+++ TEMPLATE/script.js
@@ -0,0 +1,20 @@
+// make a template and change extension settings
+
+// install prettier extension
+// file > preferences > settings > default > prettier
+// file > preferences > settings > format on save > on
+// create prettier config file > https://prettier.io/docs/en/options.html
+
+'use strict';
+
+// check blank space removal
+const x = 23;
+
+// check arrow parentheses
+const calcAge = birthYear => 2037 - birthYear;
+
+// add snippet file
+// file > preference > user snippet > global snippet
+// modify snippet to use 'log' to write the console.log() function
+
+console.log();
