f52d3c1c98953b54fa27a0c05df49e7edea95502
Tadeh Hakopian <thakopian@gmail.com>
2021-04-21 22:04:57 -0700

    html and css prep files

---
+++ BASICS-HTML-CSS/script.js
@@ -0,0 +1,39 @@
+// make a template and change extension settings
+
+// install prettier extension
+// file > preferences > settings > default > prettier
+// file > preferences > settings > format on save > on
+// create prettier config file > https://prettier.io/docs/en/options.html
+
+'use strict';
+
+// check blank space removal
+const x = 23;
+
+// check arrow parentheses
+const calcAge = birthYear => 2037 - birthYear;
+
+// add snippet file
+// file > preference > user snippet > global snippet
+// modify snippet to use 'log' to write the console.log() function
+
+console.log();
+
+// add auto close tag ext
+// add image preview ext
+// add eslint ext
+// add settings sync ext
+// add monokai pro ext
+// add todo hightlight ext
+
+// add live server ext
+// open live server in your browser
+
+// check console log in live server browser
+console.log(1950);
+
+// check terminal in script.js
+// menu > terminal / ^- > cmd+k for clear
+// check nodejs in terminal > node -v
+// install live server with npm terminal CLI > npm install live-server -g
+// check live server runnoing with gitbash CLI > liver-server
