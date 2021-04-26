dcf45288c6d875f5dac4746dc5b1e50e27a9a721
Tadeh Hakopian <47580016+thakopian@users.noreply.github.com>
2021-04-26 07:47:38 -0700

    Section 05/60
    Debugging scripts with debugger and chrome debug step through options

--- PRACTICE-PROBLEMS/devProb01.js
+++ PRACTICE-PROBLEMS/devProb01.js
@@ -72,3 +72,52 @@ const calcTempAmplitudeNew = function (t1, t2) {
 const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]); // pass the array values through the function to be concatenated
 // store amplitude value from the temperatures list in the beginning
 console.log(amplitudeNew); // log the amplitude
+
+///////////////////////////////////////
+// Debugging with the Console and Breakpoints
+
+const measureKelvin = function () {
+  const measurement = {
+    type: "temp",
+    unit: "celsius",
+    //value: Number(prompt("Degrees Celsius:")),
+    value: 10,
+  };
+
+  console.log(measurement);
+  console.table(measurement);
+
+  console.log(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+
+console.log(measureKelvin());
+
+// additional debug section with modified calcTemp function introducing bugs in last 10 minutes:
+//https://www.udemy.com/course/the-complete-javascript-course/learn/lecture/22648363#overview
+
+/*
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+  console.log(temps);
+  let max = 0;
+  let min = 0;
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// A) IDENTIFY
+console.log(amplitudeBug);
+
+const amplitudeNew = calcTempAmplitudeNew([3, 5, 1], [9, 0, 5]); // pass the array values through the function to be concatenated
+// store amplitude value from the temperatures list in the beginning
+console.log(amplitudeNew); // log the amplitude
+*/
