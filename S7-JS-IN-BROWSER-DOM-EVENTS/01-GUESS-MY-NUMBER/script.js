f54441ab0991e6972015a33a6258af9177bf2f60
Tadeh Hakopian <47580016+thakopian@users.noreply.github.com>
2021-05-24 08:57:47 -0700

    Game logic before DRY

--- S7-JS-IN-BROWSER-DOM-EVENTS/01-GUESS-MY-NUMBER/script.js
+++ S7-JS-IN-BROWSER-DOM-EVENTS/01-GUESS-MY-NUMBER/script.js
@@ -38,8 +38,9 @@ const number = Math.random();
 
 // CHECK THE FOLLOWING SEGMENT AGAINST DEMO CODE:
 
-const secretMumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretMumber;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -49,5 +50,18 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'No number!';
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--; // = score = score - 1;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = ' Too Low!';
+    score--; // = score = score - 1;
+    document.querySelector('.score').textContent = score;
   }
 });
